3d130a499368e1c2879c76f5e9d75fbb34826888
ssz256 <71969931+ssz256@users.noreply.github.com>
2022-08-31 19:35:20 +0200

    push

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
+    int i;
+    printf ("Checking if processor is available...");
+    if (system(NULL)) puts ("Ok");
+    else exit (EXIT_FAILURE);
+    printf ("Executing command DIR...\n");
+    i=system ("spwn build C:\\Users\\SZYMO\\Desktop\\dev\\tgl\\exampleScripts\\test.spwn");
+    printf ("The value returned was: %d.\n",i);
+    return 0;
     return 0;
 }
